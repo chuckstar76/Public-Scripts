c9d7fadd3d4772f6ae02f8174ce44c75931073c9
chuckstar76 <chuckstar76@gmail.com>
2020-09-15 17:54:09 +0100

    Updates to Scripttemp.sh

--- Shell-Scripts/scripttemp.sh
+++ Shell-Scripts/scripttemp.sh
@@ -1,4 +1,5 @@
 ### - Basic Shell Script Template w/License and Colour Formating - CH - September 2020 - ###
+### - Intended to be used with selfnamed configuration file for efficiency - ###
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,3 +38,21 @@ t_dim='\e[2m'
 t_undlin='\e[4m'
 t_blink='\e[5m'
 t_invert='\e[7m'
+
+### - Start Configuration Ingestion - ###
+# Activities to complete
+# Use correct reserved variables
+# Access self filename $FILENAME
+FILENAME_BEF=${0##*/}
+FILENAME=${FILENAME_BEF%.sh}
+# Access config file $CFG_FILE
+CFG_FILE=${FILENAME}.cfg
+# Access log file $LOG_FILE
+
+echo \"${red}${t_bold}🗲Starting Script Name🗲${t_reset}\"
+echo "Filename with path" - ${green}${t_bold}$0${t_reset}
+echo "Filename with path & without extension" - ${green}${t_bold}${0%.sh}${t_reset}
+echo "Filename without path" - ${green}${t_bold}${0##*/}${t_reset}
+echo "Filename without path & without extension" - ${green}${t_bold}$FILENAME${t_reset}
+echo "Configuration Filename" - ${green}${t_bold}$CFG_FILE${t_reset}
+echo \"${red}${t_bold}🏁Finished Daily Update🏁${t_reset}\" 
